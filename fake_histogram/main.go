ade5f61ecf3ec845238a2729403d1c7054d82225
misoboy <cs2024.song@partner.samsung.com>
2025-06-13 14:29:48 +0900

    fix: Error format

--- fake_histogram/main.go
+++ fake_histogram/main.go
@@ -14,7 +14,7 @@ func main() {
 		// Telegraf line protocol
 		// Here, we simulate a histogram with count field
 		// This will be interpreted as uint64 because there's no decimal point
-		fmt.Fprintf(writer, "test_histogram,host=localhost count=100,sum=123.45,bucket_le_0=10,bucket_le_10=30,bucket_le_inf=60 %d\n", timestamp)
+		fmt.Fprintf(writer, "test_histogram,host=localhost count=100u,sum=123i.45,bucket_le_0=10,bucket_le_10=30,bucket_le_inf=60 %d\n", timestamp)
 		writer.Flush()
 		time.Sleep(5 * time.Second)
 	}
